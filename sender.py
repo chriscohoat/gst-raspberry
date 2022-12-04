fb439c10e6d21e022c45ac7eee7bee5dbe3f8826
chriscohoat <chris.cohoat@gmail.com>
2022-12-03 20:25:19 -0600

    Added libcamera commands

--- sender.py
+++ sender.py
@@ -3,10 +3,11 @@ import sys
 import socket
 from PythonFileLibrary.FileReader import *
 
-## Read from configuration file first
+# Read from configuration file first
 configFile = FileReader('config')
 
-def GetSetting(fileReader : FileReader, setting : str):
+
+def GetSetting(fileReader: FileReader, setting: str):
     fileReader.ResetCursor()
 
     for line in configFile:
@@ -15,8 +16,9 @@ def GetSetting(fileReader : FileReader, setting : str):
             fileReader.MoveCursorDown()
             line = fileReader.GetCurrentLine().strip()
             return line
-    
+
     return None
+
 
 def getPrivateIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,6 +29,7 @@ def getPrivateIP():
         pass
 
     return None
+
 
 localIP = getPrivateIP()
 remoteIP = GetSetting(configFile,   '# Remote Public IP Address')
@@ -41,22 +44,15 @@ quality = GetSetting(configFile,    '# Quality (10 - 40)')
 UDPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
 TCPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
 
+UDPcommand_libcamera = "libcamera-hello --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
+TCPcommand_libcamera = "libcamera-hello --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
 
 if protocol == "TCP":
     print("Trying to send through TCP pipe...")
-    os.system(TCPcommand.format(width, height, FPS, bitrate, localIP, port, quality))
+    os.system(TCPcommand_libcamera.format(width, height, FPS,
+              bitrate, localIP, port, quality))
 
 elif protocol == "UDP":
     print("Trying to send through UDP pipe...")
-    os.system(UDPcommand.format(width, height, FPS, bitrate, remoteIP, port, quality))
-
-
-
-
-
-
-
-
-
-
-
+    os.system(UDPcommand_libcamera.format(width, height, FPS,
+              bitrate, remoteIP, port, quality))
