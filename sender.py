7eb1b831a3847d1c9da9c877015f704ef87c7972
chriscohoat <chris.cohoat@gmail.com>
2022-12-03 20:29:53 -0600

    Added libcamera-raw.

--- sender.py
+++ sender.py
@@ -44,8 +44,8 @@ quality = GetSetting(configFile,    '# Quality (10 - 40)')
 UDPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
 TCPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
 
-UDPcommand_libcamera = "libcamera-vid --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose --nopreview --raw - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
-TCPcommand_libcamera = "libcamera-vid --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose --nopreview --raw - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
+UDPcommand_libcamera = "libcamera-raw --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose --nopreview - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
+TCPcommand_libcamera = "libcamera-raw --width {0} --height {1} --framerate {2} --bitrate {3} --quality {6} --timeout 0 --verbose --nopreview - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
 
 if protocol == "TCP":
     print("Trying to send through TCP pipe...")
